221fc1209145fa944a320d5720fb16a677680d3f
sherly <sharton1@asu.edu>
2022-05-04 12:00:55 -0400

    add procfile

--- todolist.js
+++ todolist.js
@@ -236,6 +236,10 @@ app.post("/delete", function(reqFromClient, resToClient) {
 
 // 5. listen
 // allow heroku to choose port
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
